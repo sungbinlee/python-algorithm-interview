025b3e408d125a04d2ad009ed74f39f9db0be2c3
sungbinlee <sunglee@addu.edu.ph>
2023-06-29 17:53:53 +0900

    04번 문제 오탈자 수정

--- python_solutions/chapter_01_문자열_조작/problem_01_04_가장_흔한_단어.py
+++ python_solutions/chapter_01_문자열_조작/problem_01_04_가장_흔한_단어.py
@@ -1,5 +1,5 @@
 """
-Chapter 01 - Problem 02 - 로그 파일 재정렬 - https://leetcode.com/problems/most-common-word/
+Chapter 01 - Problem 04 - 가장 흔한 단어 - https://leetcode.com/problems/most-common-word/
 
 문제 설명:
 금지된 단어를 제외한 가장 흔하게 등장하는 단어를 출력하라. 대소문자 구분을 하지 않으며, 구두점(마침표, 쉼표 등) 또한 무시한다.
